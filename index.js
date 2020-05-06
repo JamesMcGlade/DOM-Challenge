957e59293cdfdf0a7fab78a791f87cd4da5c94dd
Code Nation Student <codenationstudent@codes-mbp.home>
2020-05-06 17:01:27 +0100

    Very Hot

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-// const wrapper = document.createElement('div');
-// wrapper.id = "wrapper";
-numBoxes = 9;
+let body = document.getElementsByTagName('body')[0];
+let wrapper = document.createElement('div');
+let form = document.createElement('form');
+let input = document.createElement('input');
+let submit = document.createElement('input');
+let clear = document.createElement('input');
+var box = document.createElement('div');
+var img = document.createElement('img');
+img.src = "https://source.unsplash.com/random/500x500//?bunny"
+box.className = "box";
+wrapper.id = "wrapper";
+form.id = "form";
+submit.type = "button";
+clear.type = 'button';
 
-for (i = 0; i < numBoxes; i++) {
-    var box = document.createElement('div');
-    box.className = "box";
-    wrapper.appendChild(box);
+body.appendChild(form);
+form.appendChild(input);
+form.appendChild(submit);
+form.appendChild(clear);
+body.appendChild(wrapper);
+
+submit.onclick = ()  =>{
+    numBoxes = input.value;
+    for (i = 0; i < numBoxes; i++) {
+        var box = document.createElement('div');
+        box.className = "box";
+        var img = document.createElement('img');
+        img.src = "https://source.unsplash.com/random/500x500//?bunny";
+        img.setAttribute ("height", "100vw / 3.5");
+        img.setAttribute ("width", "100vw / 3.5");
+        box.appendChild(img);
+        box.classList.add('box');
+        box.appendChild(img);
+        wrapper.appendChild(box);
+        }
+}
+
+clear.onclick = () =>{
+    const getwrap = document.getElementById('wrapper');
+    while (getwrap.lastElementChild) {
+        getwrap.removeChild(wrapper.lastElementChild);
     }
 
-
-
-
+}
 //     document.getElementById('box').appendChild(box);
 //     myBoxes = [];
 //     numBoxes = 9;
